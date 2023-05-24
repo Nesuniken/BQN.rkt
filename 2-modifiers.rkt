0022912ccce088a2da7bfa59235e0146e0718cc8
Nesuniken <nesuniken@gmail.com>
2023-05-24 02:17:14 -0500

    Fixed F⍟1 case

--- 2-modifiers.rkt
+++ 2-modifiers.rkt
@@ -58,7 +58,7 @@
   [(_ (? procedure?) _ _)
    (apply (BQN⍟ F (apply g args)) args)]
   [(_ 0 _ _) (first args)]
-  [(_ 1 _ _) (F args)]
+  [(_ 1 _ _) (apply F args)]
   [(_ (? integer?) #t _)
    (apply (BQN⍟ F (- g)) args)]
   [(_ (? exact-positive-integer?) #f _)
